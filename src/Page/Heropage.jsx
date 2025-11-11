c13db84d86b505c225f65ba258a0bb6272b8e4d7
Nur Mohammod <nurm98371@gmail.com>
2025-11-11 16:51:04 +0600

    added hero items

--- src/Page/Heropage.jsx
+++ src/Page/Heropage.jsx
@@ -3,9 +3,7 @@ import HeroImg from "./../assets/hero-food-sharing.jpg";
 
 const Heropage = () => {
   return (
-    <div>
-      <img src={HeroImg} alt="Food sharing hero"   />
-    </div>
+    
   );
 };
 
