d8044d9d3f89231538d51cbdaefd6a22f57b50c1
Nur Mohammod <nurm98371@gmail.com>
2025-11-14 19:34:04 +0600

    add

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -75,7 +75,7 @@ const Login = () => {
             <img
               src="https://www.svgrepo.com/show/475656/google-color.svg"
               alt="Google"
-              className="w-5 h-5 mr-2"
+              className="w-5 h-5 bg-green-700 mr-2"
             />
             Login with Google
           </button>
